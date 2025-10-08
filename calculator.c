4c0c1fab4bab2a44d4632a5d10401872df27c8fc
Palak Bhardwaj <avanibhardwaj30@gmail.com>
2025-10-08 16:56:57 +0530

    make changes in calculator.c

--- calculator.c
+++ calculator.c
@@ -1,113 +1,126 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdbool.h>
 
 #define SIZE 200  
 
-int isOp(char c) {
+bool isOperator(char c) {
     return (c == '+' || c == '-' || c == '*' || c == '/');
 }
 
-int valid(char s[]) {
-    for (int i = 0; s[i] != '\0'; i++) {
-        if (!(isdigit(s[i]) || isOp(s[i]) || isspace(s[i]))) {
-            return 0;
+bool isValidExpression(const char expr[]) {
+    for (int i = 0; expr[i] != '\0'; i++) {
+        unsigned char ch = (unsigned char) expr[i];
+        if (!(isdigit(ch) || isOperator(ch) || isspace(ch))) {
+            return false;
         }
     }
-    return 1;
+    return true;
 }
 
-int eval(char s[], int *err) {
-    int nums[SIZE];
-    char ops[SIZE];
-    int n = 0, o = 0, i = 0;
+int evaluateExpression(const char expr[], int *errorCode) {
+    int numbers[SIZE];
+    char operators[SIZE];
+    int numCount = 0, opCount = 0, index = 0;
 
-    while (s[i] != '\0') {
-        if (isspace(s[i])) {
-            i++;
+    while (expr[index] != '\0') {
+        unsigned char ch = (unsigned char) expr[index];
+        
+        if (isspace(ch)) {
+            index++;
             continue;
         }
-        if (isdigit(s[i])) {
-            int val = 0;
-            while (isdigit(s[i])) {
-                val = val * 10 + (s[i] - '0');
-                i++;
+
+        if (isdigit(ch)) {
+            int value = 0;
+            while (isdigit((unsigned char) expr[index])) {
+                value = value * 10 + (expr[index] - '0');
+                index++;
             }
-            nums[n++] = val;
+            numbers[numCount++] = value;
         } 
-        else if (isOp(s[i])) {
-            ops[o++] = s[i];
-            i++;
+        else if (isOperator(ch)) {
+            operators[opCount++] = expr[index];
+            index++;
         } 
         else {
-            *err = 1;
+            *errorCode = 1;
             return 0;
         }
     }
 
-    for (int j = 0; j < o; j++) {
-        if (ops[j] == '*' || ops[j] == '/') {
-            int a = nums[j];
-            int b = nums[j + 1];
+    for (int j = 0; j < opCount; j++) {
+        if (operators[j] == '*' || operators[j] == '/') {
+            int a = numbers[j];
+            int b = numbers[j + 1];
 
-            if (ops[j] == '/' && b == 0) {
-                *err = 2;
+            if (operators[j] == '/' && b == 0) {
+                *errorCode = 2;
                 return 0;
             }
 
-            int r;
-            if (ops[j] == '*')
-                r = a * b;
-            else
-                r = a / b;
-
-            nums[j] = r;
-
-            for (int k = j + 1; k < n - 1; k++) {
-                nums[k] = nums[k + 1];
+            int result;
+            if (operators[j] == '*') {
+                result = a * b;
+            } else {
+                result = a / b;
             }
-            for (int k = j; k < o - 1; k++) {
-                ops[k] = ops[k + 1];
+
+            numbers[j] = result;
+
+            for (int k = j + 1; k < numCount - 1; k++) {
+                numbers[k] = numbers[k + 1];
             }
-            n--;
-            o--;
+            for (int k = j; k < opCount - 1; k++) {
+                operators[k] = operators[k + 1];
+            }
+            numCount--;
+            opCount--;
             j--;
         }
     }
 
-    int ans = nums[0];
-    for (int j = 0; j < o; j++) {
-        if (ops[j] == '+')
-            ans += nums[j + 1];
-        else if (ops[j] == '-')
-            ans -= nums[j + 1];
+    int answer = numbers[0];
+    for (int j = 0; j < opCount; j++) {
+        if (operators[j] == '+') {
+            answer += numbers[j + 1];
+        } else if (operators[j] == '-') {
+            answer -= numbers[j + 1];
+        }
     }
 
-    return ans;
+    return answer;
 }
 
-int main() {
-    char in[SIZE];
+void calculator() {
+    char input[SIZE];
     printf("Enter an expression: ");
-    fgets(in, SIZE, stdin);
-
-    if (!valid(in)) {
-        printf("Error: Invalid expression.\n");
-        return 0;
+    if (!fgets(input, SIZE, stdin)) {
+        printf("Error reading input.\n");
+        return;
     }
 
-    int e = 0;
-    int res = eval(in, &e);
+    if (!isValidExpression(input)) {
+        printf("Error: Invalid expression.\n");
+        return;
+    }
 
-    if (e == 1) {
+    int errorCode = 0;
+    int result = evaluateExpression(input, &errorCode);
+
+    if (errorCode == 1) {
         printf("Error: Invalid expression.\n");
     } 
-    else if (e == 2) {
+    else if (errorCode == 2) {
         printf("Error: Division by zero.\n");
     } 
     else {
-        printf("%d\n", res);
+        printf("Result: %d\n", result);
     }
+}
 
+int main() {
+    Calculator();
     return 0;
 }
