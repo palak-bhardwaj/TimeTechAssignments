d4528be57c367a39364bbd3c46b5530b370bfa4d
Palak Bhardwaj <avanibhardwaj30@gmail.com>
2025-10-13 14:54:42 +0530

    Add curly braces in Student-performance-analyzer.c

--- Student-performance-analyzer.c
+++ Student-performance-analyzer.c
@@ -17,9 +17,11 @@ typedef struct {
 } Student;
 
 bool isUniqueRollNumber(Student students[], int numberOfStudents, int rollNumber) {
-    for (int i = 0; i < numberOfStudents; i++)
-        if (students[i].rollNumber == rollNumber)
+    for (int i = 0; i < numberOfStudents; i++) {
+        if (students[i].rollNumber == rollNumber) {
             return false;
+        }
+    }
     return true;
 }
 
@@ -29,8 +31,9 @@ bool isValidMarks(float marks) {
 
 float calculateTotal(Student s) {
     float total = 0;
-    for (int i = 0; i < MAX_SUBJECTS; i++)
+    for (int i = 0; i < MAX_SUBJECTS; i++) {
         total += s.marks[i];
+    }
     return total;
 }
 
@@ -39,35 +42,51 @@ float calculateAverage(Student s) {
 }
 
 Grade assignGrade(float avg) {
-    if (avg >= GRADE_A) return A;
-    else if (avg >= GRADE_B) return B;
-    else if (avg >= GRADE_C) return C;
-    else if (avg >= GRADE_D) return D;
-    else return F;
+    if (avg >= GRADE_A) {
+        return A;
+    } 
+    else if (avg >= GRADE_B) {
+        return B;
+    } 
+    else if (avg >= GRADE_C) {
+        return C;
+    } 
+    else if (avg >= GRADE_D) {
+        return D;
+    } 
+    else {
+        return F;
+    }
 }
 
 void inputStudents(Student students[], int n) {
     for (int i = 0; i < n; i++) {
         scanf("%d", &students[i].rollNumber);
-        while (!isUniqueRollNumber(students, i, students[i].rollNumber))
+        while (!isUniqueRollNumber(students, i, students[i].rollNumber)) {
             scanf("%d", &students[i].rollNumber);
+        }
+
         scanf("%s", students[i].name);
+
         for (int j = 0; j < MAX_SUBJECTS; j++) {
             scanf("%f", &students[i].marks[j]);
-            while (!isValidMarks(students[i].marks[j]))
+            while (!isValidMarks(students[i].marks[j])) {
                 scanf("%f", &students[i].marks[j]);
+            }
         }
     }
 }
 
 void sortByRoll(Student students[], int n) {
-    for (int i = 0; i < n - 1; i++)
-        for (int j = i + 1; j < n; j++)
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
             if (students[i].rollNumber > students[j].rollNumber) {
                 Student temp = students[i];
                 students[i] = students[j];
                 students[j] = temp;
             }
+        }
+    }
 }
 
 void printPerformance(Grade grade) {
@@ -79,9 +98,14 @@ void printPerformance(Grade grade) {
         case D: stars = 2; break;
         default: stars = 0; break;
     }
-    if (grade == F) return;
-    for (int i = 0; i < stars; i++)
+
+    if (grade == F) {
+        return;
+    }
+
+    for (int i = 0; i < stars; i++) {
         printf("*");
+    }
 }
 
 void displayStudents(Student students[], int n) {
@@ -89,38 +113,50 @@ void displayStudents(Student students[], int n) {
         float total = calculateTotal(students[i]);
         float avg = calculateAverage(students[i]);
         Grade grade = assignGrade(avg);
+
         printf("Roll: %d\n", students[i].rollNumber);
         printf("Name: %s\n", students[i].name);
         printf("Total: %.0f\n", total);
         printf("Average: %.2f\n", avg);
         printf("Grade: %c\n", grade);
+
         if (grade == F) {
             printf("Performance:\n");
             continue;
+        } 
+        else {
+            printf("Performance: ");
+            printPerformance(grade);
+            printf("\n");
         }
-        printf("Performance: ");
-        printPerformance(grade);
-        printf("\n");
     }
 }
 
 void printRollNumbers(Student students[], int n, int index) {
-    if (index == n) return;
-    printf("%d ", students[index].rollNumber);
-    printRollNumbers(students, n, index + 1);
+    if (index == n) {
+        return;
+    } else {
+        printf("%d ", students[index].rollNumber);
+        printRollNumbers(students, n, index + 1);
+    }
 }
 
 int main() {
     int totalStudents;
     scanf("%d", &totalStudents);
-    if (totalStudents < 1 || totalStudents > MAX_STUDENTS)
+
+    if (totalStudents < 1 || totalStudents > MAX_STUDENTS) {
         return 0;
+    }
+
     Student students[MAX_STUDENTS];
     inputStudents(students, totalStudents);
     sortByRoll(students, totalStudents);
     displayStudents(students, totalStudents);
+
     printf("List of Roll Numbers (via recursion): ");
     printRollNumbers(students, totalStudents, 0);
     printf("\n");
+
     return 0;
 }
