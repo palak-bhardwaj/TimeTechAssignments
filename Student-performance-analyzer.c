b46bada41287cf5eb0d853b26dee54569fb564ec
Palak Bhardwaj <avanibhardwaj30@gmail.com>
2025-10-13 09:11:44 +0530

    Update Student-performance-analyzer.c

--- Student-performance-analyzer.c
+++ Student-performance-analyzer.c
@@ -7,17 +7,17 @@
 #define GRADE_C 50
 #define GRADE_D 35
 
-enum Grade { A, B, C, D, F };
+typedef enum { A = 'A', B = 'B', C = 'C', D = 'D', F = 'F' } Grade;
 
-struct Student {
+typedef struct {
     int rollNumber;
     char name[50];
     float marks[MAX_SUBJECTS];
-};
+} Student;
 
-int isUniqueRoll(struct Student students[], int n, int roll) {
-    for (int i = 0; i < n; i++)
-        if (students[i].rollNumber == roll)
+int isUniqueRollNumber(Student students[], int numberOfStudents, int rollNumber) {
+    for (int i = 0; i < numberOfStudents; i++)
+        if (students[i].rollNumber == rollNumber)
             return 0;
     return 1;
 }
@@ -26,14 +26,18 @@ int isValidMarks(float marks) {
     return marks >= 0 && marks <= 100;
 }
 
-float calculateAverage(struct Student s) {
+float calculateTotal(Student s) {
     float total = 0;
     for (int i = 0; i < MAX_SUBJECTS; i++)
         total += s.marks[i];
-    return total / MAX_SUBJECTS;
+    return total;
 }
 
-enum Grade assignGrade(float avg) {
+float calculateAverage(Student s) {
+    return calculateTotal(s) / MAX_SUBJECTS;
+}
+
+Grade assignGrade(float avg) {
     if (avg >= GRADE_A) return A;
     else if (avg >= GRADE_B) return B;
     else if (avg >= GRADE_C) return C;
@@ -41,10 +45,10 @@ enum Grade assignGrade(float avg) {
     else return F;
 }
 
-void inputStudents(struct Student students[], int n) {
+void inputStudents(Student students[], int n) {
     for (int i = 0; i < n; i++) {
         scanf("%d", &students[i].rollNumber);
-        while (!isUniqueRoll(students, i, students[i].rollNumber))
+        while (!isUniqueRollNumber(students, i, students[i].rollNumber))
             scanf("%d", &students[i].rollNumber);
         scanf("%s", students[i].name);
         for (int j = 0; j < MAX_SUBJECTS; j++) {
@@ -55,31 +59,54 @@ void inputStudents(struct Student students[], int n) {
     }
 }
 
-void displayStudents(struct Student students[], int n) {
-    char gradeChars[] = {'A', 'B', 'C', 'D', 'F'};
-    for (int i = 0; i < n; i++) {
-        float avg = calculateAverage(students[i]);
-        enum Grade grade = assignGrade(avg);
-        printf("%d %s %.2f %c\n", students[i].rollNumber, students[i].name, avg, gradeChars[grade]);
-    }
-}
-
-void printRollNumbers(struct Student students[], int n, int index) {
-    if (index == n) return;
-    printf("%d ", students[index].rollNumber);
-    printRollNumbers(students, n, index + 1);
-}
-
-void sortByRoll(struct Student students[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = i + 1; j < n; j++) {
+void sortByRoll(Student students[], int n) {
+    for (int i = 0; i < n - 1; i++)
+        for (int j = i + 1; j < n; j++)
             if (students[i].rollNumber > students[j].rollNumber) {
-                struct Student temp = students[i];
+                Student temp = students[i];
                 students[i] = students[j];
                 students[j] = temp;
             }
-        }
+}
+
+void printPerformance(Grade grade) {
+    int stars = 0;
+    switch (grade) {
+        case A: stars = 5; break;
+        case B: stars = 4; break;
+        case C: stars = 3; break;
+        case D: stars = 2; break;
+        default: stars = 0; break;
     }
+    if (grade == F) return;
+    for (int i = 0; i < stars; i++)
+        printf("*");
+}
+
+void displayStudents(Student students[], int n) {
+    for (int i = 0; i < n; i++) {
+        float total = calculateTotal(students[i]);
+        float avg = calculateAverage(students[i]);
+        Grade grade = assignGrade(avg);
+        printf("Roll: %d\n", students[i].rollNumber);
+        printf("Name: %s\n", students[i].name);
+        printf("Total: %.0f\n", total);
+        printf("Average: %.2f\n", avg);
+        printf("Grade: %c\n", grade);
+        if (grade == F) {
+            printf("Performance:\n");
+            continue;
+        }
+        printf("Performance: ");
+        printPerformance(grade);
+        printf("\n");
+    }
+}
+
+void printRollNumbers(Student students[], int n, int index) {
+    if (index == n) return;
+    printf("%d ", students[index].rollNumber);
+    printRollNumbers(students, n, index + 1);
 }
 
 int main() {
@@ -87,11 +114,11 @@ int main() {
     scanf("%d", &totalStudents);
     if (totalStudents < 1 || totalStudents > MAX_STUDENTS)
         return 0;
-
-    struct Student students[MAX_STUDENTS];
+    Student students[MAX_STUDENTS];
     inputStudents(students, totalStudents);
     sortByRoll(students, totalStudents);
     displayStudents(students, totalStudents);
+    printf("List of Roll Numbers (via recursion): ");
     printRollNumbers(students, totalStudents, 0);
     printf("\n");
     return 0;
